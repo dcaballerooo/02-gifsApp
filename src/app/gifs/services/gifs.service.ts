0c019ca8c2aad9e68acbbe1632a2051870644643
dcaballerooo <daw003.2021@gmail.com>
2022-12-28 10:04:22 +0100

    Final commit
    App-terminada

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string = '236B1euiN7oZoFi68riYqxibVb1ZXndU';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[]= [];
+  
+  public resultados: Gif[]= [];
+
+  get historial(){
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient){
+
+    this._historial =  JSON.parse( localStorage.getItem('historial')! ) || []
+    this.resultados = JSON.parse(localStorage.getItem('resultados')! ) || []
+
+    // if(localStorage.getItem('historial')){
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );
+    // }
+
+  }
+  
+  buscarGifs(query: string = ''){
+    
+    query = query.trim().toLocaleLowerCase();
+
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      //Obtenemos los 10 últimos registros
+      this._historial = this._historial.splice(0,10);
+      //Grabandolo en el Local Storage
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+      
+    }
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query);
+
+
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
+    .subscribe( (resp) => {
+      this.resultados = resp.data;
+      //Grabando las img en el Local Storage
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
+    });
+
+
+  }
+}
